6d70e16eb08134a63f35df932fcf5a6a8dffdc36
Niall O Hara <niall@changex.org>
2020-01-20 15:30:41 +0000

    refactored find_note function

--- notebook_app/notebook.py
+++ notebook_app/notebook.py
@@ -2,7 +2,6 @@ import datetime
 
 last_id = 0
 
-
 class Note:
     """Represent a note in the notebook. Match string in searches and store tags for each note."""
 
@@ -19,7 +18,6 @@ class Note:
         return filter in self.memo or filter in self.tags
 
 
-
 class Notebook:
     """Represents a collection of notes that can be tagged, modified and searched"""
 
@@ -32,16 +30,17 @@ class Notebook:
 
     def modify_memo(self, note_id, memo):
         """Find the note with the given id and change its memo to the given value."""
-        for note in self.notes:
-            if note.id == note_id:
-                note.memo = memo
-                break
+        self._find_note(note_id).memo = memo
 
     def modify_tags(self, note_id, tags):
+        self._find_note(note_id).tags = tags
+
+    def _find_note(self, note_id):
+        """Locate the note with the given id."""
         for note in self.notes:
             if note.id == note_id:
-                note.tags = tags
-                break
+                return note
+        return None
 
     def search(self, filter):
         """Find all notes that match given filter string"""
